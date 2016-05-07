e31ad5a7d84c9aa964202296f64abea404a053aa
Roman Dunets <roman.dunets.email@gmail.com>
2016-05-07 13:09:56 +0300

    Bind title to input field

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -6,13 +6,19 @@ import Header from "./Header";
 export default class Layout extends React.Component {
   constructor() {
     super();
-    this.state = { title: "Welcome!" };
+    this.state = {
+      title: "Welcome!"
+    };
+  }
+
+  changeTitle(title) {
+    this.setState({title});
   }
 
   render() {
     return (
       <div>
-        <Header title={this.state.title} />
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
         <Footer />
       </div>
     );
