132abd34a16a55b1f6fe0efe442e71269fc5b829
Roman Dunets <roman.dunets.email@gmail.com>
2016-05-13 19:32:10 +0300

    Move Layout.js to pages

--- src/js/client.js
+++ src/js/client.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-import Layout from "./components/Layout";
+import Layout from "./pages/Layout";
 
 const app = document.getElementById('app');
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Layout}></Route>
