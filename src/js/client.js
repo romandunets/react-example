00ef85d6450b41a2c9751420e0bf6a7f9926186f
Roman Dunets <roman.dunets.email@gmail.com>
2016-05-14 19:32:10 +0300

    Add Groups subroute

--- src/js/client.js
+++ src/js/client.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-import Layout from "./components/Layout";
+import Layout from "./pages/Layout";
+import Groups from "./pages/Groups";
 
 const app = document.getElementById('app');
 
 ReactDOM.render(
   <Router history={hashHistory}>
-    <Route path="/" component={Layout}></Route>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Groups}></IndexRoute>
+    </Route>
   </Router>,
 app);
