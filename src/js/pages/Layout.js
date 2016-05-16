3bbdcc22697648bd7492432f256f844302edf556
Roman Dunets <roman.dunets.email@gmail.com>
2016-05-16 21:14:46 +0300

    Move navigation to separate component file

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Link } from "react-router";
 
+import Navigation from "../components/Navigation";
+
 export default class Layout extends React.Component {
   render() {
     return (
       <div>
-        <h1>Welcome to KnookApp</h1>
-        <p class="lead">This is a basic notes application intended to demonstrate how to use ReactJS</p>
-        {this.props.children}
-        <Link to="groups">My Groups</Link>
-        <Link to="notes">My Notes</Link>
-        <Link to="profile">My Profile</Link>
+        <Navigation />
+
+        <div>
+          <h1>Welcome to KnookApp</h1>
+          <p class="lead">This is a basic notes application intended to demonstrate how to use ReactJS</p>
+          {this.props.children}
+          <Link to="groups">My Groups</Link>
+          <Link to="notes">My Notes</Link>
+          <Link to="profile">My Profile</Link>
+        </div>
       </div>
     );
   }
