bff23b7aa247ebf58f367c14e803ada00794d702
Roman Dunets <roman.dunets.email@gmail.com>
2016-05-22 19:32:10 +0300

    Add table layout to groups page

--- src/js/pages/Groups.js
+++ src/js/pages/Groups.js
@@ -4,7 +4,26 @@ export default class Groups extends React.Component {
   render() {
     return (
       <div>
-        <h1>My Groups</h1>
+        <h2>My Groups</h2>
+        <table class="table">
+          <tbody>
+            <tr>
+              <td>Studies</td>
+              <td>This is group about studies</td>
+              <td>23 notes</td>
+            </tr>
+            <tr>
+              <td>Work</td>
+              <td>This is group about work</td>
+              <td>12 notes</td>
+            </tr>
+            <tr>
+              <td>Hobby</td>
+              <td>This is group about hobby</td>
+              <td>1 note</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
