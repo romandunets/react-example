b5d88b9ebf5ee12f04a5853c27c313a8a10d3e28
Roman Dunets <roman.dunets.email@gmail.com>
2016-06-07 19:32:10 +0300

    Add create note action constants

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -3,5 +3,9 @@ import keyMirror from 'keymirror';
 export default keyMirror({
   LIST_NOTES_REQUEST: null,
   LIST_NOTES_SUCCESS: null,
-  LIST_NOTES_FAILURE: null
+  LIST_NOTES_FAILURE: null,
+
+  CREATE_NOTE_REQUEST: null,
+  CREATE_NOTE_SUCCESS: null,
+  CREATE_NOTE_FAILURE: null,
 });
