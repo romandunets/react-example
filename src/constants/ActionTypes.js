b6943996c17501fcfff85135209160ec202dd4c9
Roman Dunets <roman.dunets.email@gmail.com>
2016-06-07 19:32:10 +0300

    Add get note action constants

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -5,6 +5,10 @@ export default keyMirror({
   LIST_NOTES_SUCCESS: null,
   LIST_NOTES_FAILURE: null,
 
+  GET_NOTE_REQUEST: null,
+  GET_NOTE_SUCCESS: null,
+  GET_NOTE_FAILURE: null,
+
   CREATE_NOTE_REQUEST: null,
   CREATE_NOTE_SUCCESS: null,
   CREATE_NOTE_FAILURE: null,
