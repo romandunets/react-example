410eac4702323e288dcf18cf47f948bb87ff0bb8
Roman Dunets <roman.dunets.email@gmail.com>
2016-06-15 19:32:10 +0300

    Refactor groups list page

--- src/containers/groups/GroupsListPage.js
+++ src/containers/groups/GroupsListPage.js
@@ -1,40 +1,6 @@
-/*import React, { Component } from 'react';
-
-class GroupsListPage extends Component {
-  render() {
-    return (
-      <div>
-        <table class="table">
-          <tbody>
-            <tr>
-              <td>Studies</td>
-              <td>This is group about studies</td>
-              <td>23 notes</td>
-            </tr>
-            <tr>
-              <td>Work</td>
-              <td>This is group about work</td>
-              <td>12 notes</td>
-            </tr>
-            <tr>
-              <td>Hobby</td>
-              <td>This is group about hobby</td>
-              <td>1 note</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
-
-export default GroupsListPage;*/
-
-
-
 import React, { Component } from 'react';
 
-import Group from '../../components/groups/Group';
+import GroupListItem from '../../components/groups/GroupListItem';
 import * as GroupsActions from '../../actions/GroupsActions';
 import GroupsStore from '../../stores/GroupsStore';
 
