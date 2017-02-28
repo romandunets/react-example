565a1d822f5d3ad9e26a81aff6699a03ea633a2c
Roman Dunets <roman.dunets.email@gmail.com>
2017-02-28 22:47:19 +0300

    Add UseActions class

---
+++ src/actions/UsersActions.js
@@ -0,0 +1,23 @@
+import AppDispatcher from '../dispatcher/AppDispatcher';
+import ActionTypes from '../constants/ActionTypes';
+import userApi from '../api/UserApi';
+
+export function listUsers() {
+  AppDispatcher.dispatch({
+    actionType: ActionTypes.LIST_USERS_REQUEST
+  });
+
+  userApi.listUsers()
+    .then(function (response) {
+      AppDispatcher.dispatch({
+        actionType: ActionTypes.LIST_USERS_SUCCESS,
+        notes: response.data
+      });
+    })
+    .catch(function (error) {
+      AppDispatcher.dispatch({
+        actionType: ActionTypes.LIST_USERS_FAILURE,
+        error: error
+      });
+    });
+}
