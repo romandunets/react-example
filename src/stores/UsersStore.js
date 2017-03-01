0615637e9e0458548a4cc2155a764fe3d6843ed7
Roman Dunets <roman.dunets.email@gmail.com>
2017-03-01 19:21:42 +0300

    Add users store class

---
+++ src/stores/UsersStore.js
@@ -0,0 +1,22 @@
+import ActionTypes from '../constants/ActionTypes';
+import AppDispatcher from '../dispatcher/AppDispatcher';
+import { createStore, merge } from '../utils/StoreUtils';
+
+const _users = [];
+
+const UserStore = createStore({
+  listUsers() {
+    return _users;
+  }
+});
+
+UserStore.dispatchToken = AppDispatcher.register(action => {
+  switch(action.actionType) {
+    case ActionTypes.LIST_USERS_SUCCESS:
+      merge(_users, action.users);
+      UserStore.emitChange();
+      break;
+  }
+});
+
+export default UserStore;
