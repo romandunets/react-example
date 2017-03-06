36e066133314b7b15bec0eb599e8a435b0d67e9f
Roman Dunets <roman.dunets.email@gmail.com>
2017-03-06 22:52:13 +0200

    Add getUser to userApi class

--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -4,6 +4,10 @@ class UserApi extends Api {
   static listUsers() {
     return this.getClient().get('/users');
   }
+
+  static getUser(id) {
+    return this.getClient().get(`/users/${id}`);
+  }
 }
 
 export default UserApi;
