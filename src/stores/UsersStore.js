9cb4a09ce9fc194000f01d2ddae4d60c7e37d9e6
Roman Dunets <roman.dunets.email@gmail.com>
2017-03-07 21:25:47 +0300

    Add UserStore

--- src/stores/UsersStore.js
+++ src/stores/UsersStore.js
@@ -3,10 +3,14 @@ import AppDispatcher from '../dispatcher/AppDispatcher';
 import { createStore, merge } from '../utils/StoreUtils';
 
 const _users = [];
+const _user = {};
 
 const UserStore = createStore({
   listUsers() {
     return _users;
+  },
+  getUser() {
+    return _user;
   }
 });
 
@@ -14,6 +18,10 @@ UserStore.dispatchToken = AppDispatcher.register(action => {
   switch(action.actionType) {
     case ActionTypes.LIST_USERS_SUCCESS:
       merge(_users, action.data);
+      UserStore.emitChange();
+      break;
+    case ActionTypes.GET_USER_SUCCESS:
+      merge(_user, action.data);
       UserStore.emitChange();
       break;
   }
