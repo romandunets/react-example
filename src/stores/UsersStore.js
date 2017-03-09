ff2661417849a1810d8d97a5d7bba3e15c6b29d9
Roman Dunets <roman.dunets.email@gmail.com>
2017-03-09 16:17:39 +0300

    Fix getting user data in userStore

--- src/stores/UsersStore.js
+++ src/stores/UsersStore.js
@@ -2,8 +2,8 @@ import ActionTypes from '../constants/ActionTypes';
 import AppDispatcher from '../dispatcher/AppDispatcher';
 import { createStore, merge } from '../utils/StoreUtils';
 
-const _users = [];
-const _user = {};
+let _users = [];
+let _user = {};
 
 const UserStore = createStore({
   listUsers() {
@@ -17,11 +17,11 @@ const UserStore = createStore({
 UserStore.dispatchToken = AppDispatcher.register(action => {
   switch(action.actionType) {
     case ActionTypes.LIST_USERS_SUCCESS:
-      merge(_users, action.data);
+      _users = action.data;
       UserStore.emitChange();
       break;
     case ActionTypes.GET_USER_SUCCESS:
-      merge(_user, action.data);
+      _user = action.data;
       UserStore.emitChange();
       break;
   }
