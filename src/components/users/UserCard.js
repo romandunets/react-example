11df6c81af51891065f46a066063ef65d237c4c2
Roman Dunets <roman.dunets.email@gmail.com>
2017-03-09 16:17:39 +0300

    Add basic styling for user card using bootstrap

--- src/components/users/UserCard.js
+++ src/components/users/UserCard.js
@@ -5,15 +5,15 @@ class UserCard extends Component {
     const { user } = this.props;
 
     return (
-      <dl>
-        <dt>Username</dt>
-        <dd>{ user.username }</dd>
-        <dt>First name</dt>
-        <dd>{ user.firstName }</dd>
-        <dt>Last name</dt>
-        <dd>{ user.lastName }</dd>
-        <dt>Role</dt>
-        <dd>{ user.role }</dd>
+      <dl class="row">
+        <dt class="col-sm-3">Username</dt>
+        <dd class="col-sm-9">{ user.username }</dd>
+        <dt class="col-sm-3">First name</dt>
+        <dd class="col-sm-9">{ user.firstName }</dd>
+        <dt class="col-sm-3">Last name</dt>
+        <dd class="col-sm-9">{ user.lastName }</dd>
+        <dt class="col-sm-3">Role</dt>
+        <dd class="col-sm-9">{ user.role }</dd>
       </dl>
     );
   }
