2b846f000155338f4d9dbd9930aef588752dec6d
Roman Dunets <roman.dunets.email@gmail.com>
2017-03-10 16:17:39 +0300

    Add API call for getting single note details

--- src/api/NoteApi.js
+++ src/api/NoteApi.js
@@ -4,6 +4,10 @@ class NoteApi extends Api {
   static listNotes() {
     return this.getClient().get('/notes');
   }
+
+  static getNote(id) {
+    return this.getClient().get(`/notes/${id}`);
+  }
 }
 
 export default NoteApi;
