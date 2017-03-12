4c93e88e5277da4a0790c128ad7bd1c0b2f0e549
Roman Dunets <roman.dunets.email@gmail.com>
2017-03-12 14:38:47 +0300

    Add action handler for get note in NotesStore

--- src/stores/NotesStore.js
+++ src/stores/NotesStore.js
@@ -2,11 +2,15 @@ import ActionTypes from '../constants/ActionTypes';
 import AppDispatcher from '../dispatcher/AppDispatcher';
 import { createStore, merge } from '../utils/StoreUtils';
 
-const _notes = [];
+let _notes = [];
+let _note = {};
 
 const NoteStore = createStore({
   listNotes() {
     return _notes;
+  },
+  getNote() {
+    return _note;
   }
 });
 
@@ -14,6 +18,10 @@ NoteStore.dispatchToken = AppDispatcher.register(action => {
   switch(action.actionType) {
     case ActionTypes.LIST_NOTES_SUCCESS:
       merge(_notes, action.data);
+      NoteStore.emitChange();
+      break;
+    case ActionTypes.GET_NOTE_SUCCESS:
+      _note = action.data;
       NoteStore.emitChange();
       break;
   }
