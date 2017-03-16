b2057718f0d5fe9dbc1e200e1fdac988deac68d1
Roman Dunets <roman.dunets.email@gmail.com>
2017-03-16 22:01:19 +0300

    Refactor links in users list

--- src/components/users/UserListItem.js
+++ src/components/users/UserListItem.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { Link, browserHistory } from 'react-router';
 
 class UserListItem extends Component {
   render() {
     const { ...user } = this.props;
 
     return (
-      <tr>
-        <td><Link to={`/users/${user.id}`}>{ user.username }</Link></td>
+      <tr onClick={ (e) => browserHistory.push(`users/${user.id}`) }>
+        <td>{ user.username }</td>
       </tr>
     );
   }
