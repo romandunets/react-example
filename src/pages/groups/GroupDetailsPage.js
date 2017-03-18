e4c94b323f0d46c12d62af121cdf7393ab6cfaba
Roman Dunets <roman.dunets.email@gmail.com>
2017-03-18 15:59:41 +0200

    Add groups detail page

---
+++ src/pages/groups/GroupDetailsPage.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import * as GroupsActions from '../../actions/GroupsActions';
+import GroupsStore from '../../stores/GroupsStore';
+import GroupDetails from '../../components/groups/GroupDetails';
+
+class GroupDetailsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      group: {}
+    }
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentWillMount() {
+    GroupsStore.addChangeListener(this.onChange);
+  }
+
+  componentDidMount() {
+    GroupsActions.getGroup(this.props.params.id);
+  }
+
+  componentWillUnmount() {
+    GroupsStore.removeChangeListener(this.onChange);
+  }
+
+  onChange() {
+    this.setState({
+      group: GroupsStore.getNote()
+    });
+  }
+
+  render() {
+    const { group } = this.state;
+
+    return (
+      <GroupDetails group={ group } />
+    );
+  }
+}
+
+export default GroupDetailsPage;
