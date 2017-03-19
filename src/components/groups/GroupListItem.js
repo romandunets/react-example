aed18261471021501f54ff30461c2d605605d0a8
Roman Dunets <roman.dunets.email@gmail.com>
2017-03-19 10:43:34 +0200

    Add redirecting on group list item click

--- src/components/groups/GroupListItem.js
+++ src/components/groups/GroupListItem.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
 
 class GroupListItem extends Component {
   render() {
     const { ...group } = this.props;
 
     return (
-      <tr>
+      <tr onClick={ (e) => browserHistory.push(`groups/${group.id}`) }>
         <td>{ group.name }</td>
         <td>{ group.description }</td>
         <td>{ group.notesCount } notes</td>
